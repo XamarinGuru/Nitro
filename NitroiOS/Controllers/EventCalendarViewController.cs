583c3051c5d203e9ccbde15160a2ef10bd8961db
Erlend Frendsen <erlend0720@hotmail.com>
2017-02-24 03:25:46 -0800

    iOS calendar date issue

--- NitroiOS/Controllers/EventCalendarViewController.cs
+++ NitroiOS/Controllers/EventCalendarViewController.cs
@@ -130,13 +130,70 @@ namespace location2
 				var startDate = nitroEvent.StartDateTime();
 				var endDate = nitroEvent.EndDateTime();
 
-				var eventDetail = new EventDetails(ConvertDateTimeToNSDate(startDate), ConvertDateTimeToNSDate(endDate), nitroEvent.title);
+				var eventDetail = new EventDetails((NSDate)(ConvertUTCToLocalTimeZone(startDate)), (NSDate)(ConvertUTCToLocalTimeZone(endDate)), nitroEvent.title);
 				eventDetails[i] = eventDetail;
 			}
 
 			_calendar.EventSchedule = eventDetails;
 
 		}
+
+
+
+
+
+
+
+
+		public DateTime ConvertUTCToLocalTimeZone(DateTime dateTimeUtc)
+		{
+			NSTimeZone sourceTimeZone = new NSTimeZone("UTC");
+			NSTimeZone destinationTimeZone = NSTimeZone.LocalTimeZone;
+			NSDate sourceDate = DateTimeToNativeDate(dateTimeUtc);
+
+			int sourceGMTOffset = (int)sourceTimeZone.SecondsFromGMT(sourceDate);
+			int destinationGMTOffset = (int)destinationTimeZone.SecondsFromGMT(sourceDate);
+			int interval = sourceGMTOffset - destinationGMTOffset;
+
+			var destinationDate = dateTimeUtc.AddSeconds(interval);
+			//var destinationDate = sourceDate.AddSeconds(interval);
+			//var dateTime = NativeDateToDateTime(destinationDate);
+			return destinationDate;
+		}
+
+		/// <summary>
+		/// Converts a System.DateTime to an NSDate
+		/// </summary>
+		/// <returns>The time to native date.</returns>
+		/// <param name="date">Date.</param>
+		public static NSDate DateTimeToNativeDate(DateTime date)
+		{
+			DateTime reference = TimeZone.CurrentTimeZone.ToLocalTime(
+				new DateTime(2001, 1, 1, 0, 0, 0));
+			return NSDate.FromTimeIntervalSinceReferenceDate(
+				(date - reference).TotalSeconds);
+		}
+
+		/// <summary>
+		/// Converts a NSDate to System.DateTime
+		/// </summary>
+		/// <returns>The date to date time.</returns>
+		/// <param name="date">Date.</param>
+		public static DateTime NativeDateToDateTime(NSDate date)
+		{
+			DateTime reference = TimeZone.CurrentTimeZone.ToLocalTime(
+				new DateTime(2001, 1, 1, 0, 0, 0));
+			return reference.AddSeconds(date.SecondsSinceReferenceDate);
+		}
+
+
+
+
+
+
+
+
+
 
 		void FilterEventsByDate(DateTime filterDate)
 		{
