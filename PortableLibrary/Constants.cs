6a6ace0da4d17c89f20a1d70a2572d1966301624
Xamairn Guru <xamairnguru@gmail.com>
2017-04-12 13:38:38 -0700

    new terms url

--- PortableLibrary/Constants.cs
+++ PortableLibrary/Constants.cs
@@ -17,7 +17,7 @@ namespace PortableLibrary
 		public const string GOHEJA_BASEPATH = "http://go-heja.com/Service1.svc?wsdl";
 
 		//URLs
-		public const string URL_TERMS = "http://go-heja.com/nitro/terms.php/";
+		public const string URL_TERMS = "http://go-heja.com/general/terms.html";
 		public const string URL_EVENT_MAP = "http://go-heja.com/nitro/calenPage.php?name={0}&startdate={1}&user={2}";
 		public const string URL_ANALYTICS_MAP = "http://go-heja.com:8080/nitro/mobongoing.php?txt={0}";
 		public const string URL_CALENDAR = "http://go-heja.com/nitro/mobda.php?userNickName={0}&userId={1}";
